4d96d8444b03aa9658f948ec16a82be474b20b6f
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2020-03-13 20:17:12 +0530

    List and Grid views setup.

---
+++ components/ListView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class ListView extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log(this.props)
+        return (
+            <View>
+
+            </View>
+        );
+    }
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Avatar, Text } from 'react-native-elements';
+import { Avatar, Text, ButtonGroup } from 'react-native-elements';
+import ListView from '../components/ListView';
+import GridView from '../components/GridView';
+
+const views = {
+    0: <ListView events={'yo'}/>,
+    1: <GridView events={'hey'}/>
+}
 
 export default class HomeScreen extends Component {
+    constructor () {
+        super()
+        this.state = {
+          selectedIndex: 0
+        }
+      }
+
+    updateIndex =  (selectedIndex) => this.setState({selectedIndex})
+      
     render() {
         const name = this.props.route.params.name;
         const firstLetter = name[0]
         const lastName = name.split(" ") !== [] ? name.split(" ")[1][0] : ''
+        const buttons = ['List', 'Grid']
+        const { selectedIndex } = this.state
         return (
             <View style={styles.container}>
                 <Avatar
                     size="xlarge"
                     rounded
                     title={firstLetter+lastName}
-                    onPress={() => console.log("Works!")}
                     activeOpacity={0.7}
                 />
                 <Text h3>
                     {name}
                 </Text>
+                <ButtonGroup
+                    onPress={this.updateIndex}
+                    selectedIndex={selectedIndex}
+                    buttons={buttons}
+                    containerStyle={{height: 40}}
+                />
+                {views[selectedIndex]}
             </View>
         );
     }
